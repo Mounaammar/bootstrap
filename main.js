0399d705c9dc0a5f352657bfb6c90c41f5f5993d
Mounaammar <46628537+Mounaammar@users.noreply.github.com>
2019-03-17 18:05:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+ $('.icone i').on('mouseenter', function(e)  {
+    $(e.currentTarget.nextSibling).toggleClass('show')
+})
+  $('.icone i').on('mouseleave', function(e)  {
+    $(e.currentTarget.nextSibling).toggleClass('show')
+ })
